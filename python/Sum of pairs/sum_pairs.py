3b7ae9c64017d5dc223c53ce5ec80d74a034f5b1
danlee65071 <danlee65071@gmail.com>
2021-04-04 01:10:55 +0300

    Sum of pairs

--- python/Sum of pairs/sum_pairs.py
+++ python/Sum of pairs/sum_pairs.py
@@ -4,3 +4,12 @@ def sum_pairs(ints, s):
         if s - i in cache_set:
             return [s - i, i]
         cache_set.add(i)
+
+'''def sum_pairs(ints, s):
+    for i in range(1, len(ints)):
+        subints = ints[: i]
+        if s - ints[i] in subints:
+            return [s - ints[i], ints[i]]'''
+
+
+print(sum_pairs([10, 5, 2, 3, 7, 5], 10))
